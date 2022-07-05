e6d790fa3afb7b2faf91a0325350fa961b8819a1
M Haidar Hanif <git@mhaidarhanif.com>
2022-07-06 04:19:10 +0700

    feat: layout and home hero

--- src/contents/home/hero.tsx
+++ src/contents/home/hero.tsx
@@ -1,29 +1,39 @@
 export function HomeHero() {
   return (
-    <div className="max-w-lg space-y-6 py-10">
-      <header className="space-y-2">
-        <h1 className="sm:text-xl font-bold uppercase text-teal-600">
-          Belajar Dengan Catamyst
-        </h1>
-        <h2 className="text-3xl sm:text-5xl font-bold">
-          Menguasai Frontend Web Development
-        </h2>
-      </header>
-      <div className="space-y-2">
-        <p>
-          Ayo kita belajar web development dengan langsung dimentori oleh
-          ahlinya!
-        </p>
-        <p>
-          Mulai dari prinsip fundamental hingga teknik yang modern. Agar hasil
-          website dan aplikasimu bisa canggih dan juga nyaman.
-        </p>
-        <p>
-          Dengan alur kerja profesional, membantumu membuat proyek nyata,
-          mendapatkan pekerjaan, melakukan freelancing, memulai tech business,
-          bahkan kerja online secara remote.
-        </p>
+    <section className="flex gap-10 flex-wrap justify-between items-center">
+      <div className="max-w-lg space-y-6 py-10">
+        <header className="space-y-2">
+          <h1 className="sm:text-xl font-bold uppercase text-teal-600">
+            Belajar Dengan Catamyst
+          </h1>
+          <h2 className="text-3xl sm:text-5xl font-bold">
+            Menguasai Frontend Web Development
+          </h2>
+        </header>
+        <div className="space-y-2">
+          <p>
+            Ayo kita belajar web development dengan langsung dimentori oleh
+            ahlinya!
+          </p>
+          <p>
+            Mulai dari prinsip fundamental hingga teknik yang modern. Agar hasil
+            website dan aplikasimu bisa canggih dan juga nyaman.
+          </p>
+          <p>
+            Dengan alur kerja profesional, membantumu membuat proyek nyata,
+            mendapatkan pekerjaan, melakukan freelancing, memulai tech business,
+            bahkan kerja online secara remote.
+          </p>
+        </div>
       </div>
-    </div>
+
+      <div>
+        <img
+          className="max-w-[400px]"
+          src="https://program.codepolitan.com/wp-content/uploads/sites/2/2022/05/sample.png"
+          alt="Home Hero"
+        />
+      </div>
+    </section>
   );
 }
--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -3,5 +3,9 @@ interface LayoutProps {
 }
 
 export function Layout({ children }: LayoutProps) {
-  return <div className="px-2 sm:px-5">{children}</div>;
+  return (
+    <div className="px-2 sm:px-5 flex justify-center">
+      <div className="max-w-5xl w-full">{children}</div>
+    </div>
+  );
 }
